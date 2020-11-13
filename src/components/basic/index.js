6b8f657d30c549766c0da04644c8cab659f7c64a
Alex Martin <atonymartin20@yahoo.com>
2020-11-13 12:49:37 -0600

    Basic index.js cleaned up

--- src/components/basic/index.js
+++ src/components/basic/index.js
@@ -1,46 +1,74 @@
 import React from 'react';
-import { BasicOutsideDiv, BasicScoreTitle, BasicScoreOutsideDiv, BasicScoreInsideDiv, BasicScoreInsideP, BasicScoreInsideCounter, BasicMiddleDiv, BasicPaperScissorsDiv, PaperButton, RockButton, ScissorsButton, BasicBottomDiv, SwitchDifficultyButton, ShowRulesButton } from '../styledComponents';
-import Rules from '../rules';
-import Title from '../../images/logo.svg';
-
+import { BasicOutsideDiv } from '../styledComponents/basic.js';
+import ScoreSection from './scoreSection.js';
+import Step0 from './step0.js';
+import Step1 from './step1.js';
+import BottomSection from '../rules/bottomSection.js';
 
 class Basic extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-
+            userChoiceMade: false,
+            userChoseRock: false,
+            userChosePaper: false,
+            userChoseScissors: false,
+            computerChoiceMade: false,
+            computerChoseRock: false,
+            computerChosePaper: false,
+            computerChoseScissors: false,
         }
         console.log(props)
     }
 
-    render() {
-        return (
-            <BasicOutsideDiv>
-                <BasicScoreOutsideDiv>
-                    <BasicScoreTitle src={Title} alt='Title' />
-                    <BasicScoreInsideDiv>
-                        <BasicScoreInsideP>
-                            SCORE
-                        </BasicScoreInsideP>
-                        <BasicScoreInsideCounter>
-                            {this.props.score}
-                        </BasicScoreInsideCounter>
-                    </BasicScoreInsideDiv>
-                </BasicScoreOutsideDiv>
-                <BasicMiddleDiv>
-                    <BasicPaperScissorsDiv>
-                        <PaperButton></PaperButton>
-                        <ScissorsButton></ScissorsButton>
+    userChoosesRock = () => {
+        this.setState({
+            userChoiceMade: !this.state.userChoiceMade,
+            userChoseRock: true,
+        })
+        console.log('rock')
+    }
 
-                    </BasicPaperScissorsDiv>
-                    <RockButton></RockButton>
-                </BasicMiddleDiv>
-                <BasicBottomDiv>
-                    <SwitchDifficultyButton>ADVANCED</SwitchDifficultyButton>
-                    <ShowRulesButton>RULES</ShowRulesButton>
-                </BasicBottomDiv>
-            </BasicOutsideDiv>
-        )
+    userChoosesPaper = () => {
+        this.setState({
+            userChoiceMade: !this.state.userChoiceMade,
+            userChosePaper: true,
+        })
+        console.log('paper')
+    }
+
+    userChoosesScissors = () => {
+        this.setState({
+            userChoiceMade: !this.state.userChoiceMade,
+            userChoseScissors: true,
+        })
+        console.log('scissors')
+    }
+
+    render() {
+        if(this.state.choicePicked === false) {
+            return (
+                <BasicOutsideDiv>
+                    <ScoreSection score={this.props.score} />
+                    <Step0 userChoiceMade={this.state.userChoiceMade} userChoseRock={this.state.userChoseRock} userChosePaper={this.state.userChosePaper} userChoseScissors={this.state.userChoseScissors} userChoosesRock={this.userChoosesRock} userChoosesPaper={this.userChoosesPaper} userChoosesScissors={this.userChoosesScissors} />
+                    <BottomSection showBasic={this.props.showBasic} showAdvanced={this.props.showAdvanced} switchMode={this.props.switchMode} />
+                </BasicOutsideDiv>
+            )
+        }
+        else if(this.state.computerChoiceMade === false) {
+            return (
+                <BasicOutsideDiv>
+                    <ScoreSection score={this.props.score} />
+                    <Step1 userChoiceMade={this.state.userChoiceMade} userChoseRock={this.state.userChoseRock} userChosePaper={this.state.userChosePaper} userChoseScissors={this.state.userChoseScissors} userChoosesRock={this.userChoosesRock} userChoosesPaper={this.userChoosesPaper} userChoosesScissors={this.userChoosesScissors}/>
+                    <BottomSection showBasic={this.props.showBasic} showAdvanced={this.props.showAdvanced} switchMode={this.props.switchMode} />
+                </BasicOutsideDiv>
+            )
+        }
+
+        else {
+            
+        }
+
     }
 }
 
