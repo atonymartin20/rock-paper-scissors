abdba1a675764467ae93f088b3c10b10690c027a
Alex Martin <atonymartin20@yahoo.com>
2020-11-13 12:27:09 -0600

    Update index.js

--- src/components/homepage/index.js
+++ src/components/homepage/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HomepageOutsideDiv, HomepageInsideDiv } from '../styledComponents';
+import { HomepageOutsideDiv, HomepageInsideDiv } from '../styledComponents/homepage.js';
 import Basic from '../basic';
 import Advanced from '../advanced';
 
@@ -12,27 +12,19 @@ class Homepage extends React.Component {
         showBasic: true,
     }
 
-    switchRules = () => {
+    switchMode = () => {
         this.setState ({
             showAdvanced: !this.state.showAdvanced,
             showBasic: !this.state.showBasic
         })
     }
 
-    // score outside box
-    // 2px border with border-radius x
-    // width 700px
-    // height 152px
-    // centered on page 47px from top
-
-    // score inside box
-    // STILLL TO GRAB
     render() {
         if(this.state.showBasic === true) {
             return (
                 <HomepageOutsideDiv>
                     <HomepageInsideDiv>
-                        <Basic score={this.state.basicScore} showRules={this.state.showRules} showAdvanced={this.state.showAdvanced} showBasic={this.state.showBasic} switchRules={this.switchRules} />
+                        <Basic score={this.state.basicScore} showRules={this.state.showRules} showAdvanced={this.state.showAdvanced} showBasic={this.state.showBasic} switchMode={this.switchMode} />
                     </HomepageInsideDiv>
                 </HomepageOutsideDiv>
             )
@@ -41,7 +33,7 @@ class Homepage extends React.Component {
             return (
                 <HomepageOutsideDiv>
                     <HomepageInsideDiv>
-                        <Advanced score={this.state.advancedScore} showRules={this.state.showRules} showAdvanced={this.state.showAdvanced} showBasic={this.state.showBasic} switchRules={this.switchRules} />
+                        <Advanced score={this.state.advancedScore} showRules={this.state.showRules} showAdvanced={this.state.showAdvanced} showBasic={this.state.showBasic} switchMode={this.switchMode} />
                     </HomepageInsideDiv>
                 </HomepageOutsideDiv>
             )
@@ -50,22 +42,4 @@ class Homepage extends React.Component {
     }
 }
 
-// middle
-// 60px down from bottom of score box
-
-
-// rules box
-// height 40px
-// 30px from bottom and right
-// width: 128px
-
 export default Homepage;
-//     constructor(props) {
-    // super(props);
-    // this.state = {
-    //     userData: {},
-    //     disablePickUpButton: false,
-    // }
-// <InventoryList ListType='items' />
-//   if (this.props.ListType === "players") {
-//  }
