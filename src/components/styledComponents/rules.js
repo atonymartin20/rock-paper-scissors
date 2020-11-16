d40ca0d78a7382a7d73331b1f9eac53d2d5fd806
Alex Martin <atonymartin20@yahoo.com>
2020-11-16 10:34:41 -0600

    Rules section styled

---
+++ src/components/styledComponents/rules.js
@@ -0,0 +1,103 @@
+import styled from 'styled-components';
+import '../../index.css';
+
+const RulesBottomDiv = styled.div`
+    width: 100%;
+    height: 100px;
+    margin-bottom: 30px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    padding: 0px 32px;
+
+    @media(max-width: 375px) {
+        flex-direction: column-reverse;
+        justify-content: flex-end;
+        align-items: center;
+        height: 150px;
+    }
+`;
+
+const SwitchDifficultyButton = styled.button`
+    background-color: inherit;
+    border: 1px solid white;
+    color: #D5D7E3;
+    height: 40px;
+    font-size: 1.6rem;
+    border-radius: 7px;
+    width: 128px;
+    letter-spacing: 2.5px;
+    font-family: 'Barlow Semi Condensed', serif;
+`;
+
+const ShowRulesButton = styled.button`
+    background-color: inherit;
+    border: 1px solid white;
+    color: #D5D7E3;
+    height: 40px;
+    font-size: 1.6rem;
+    border-radius: 7px;
+    width: 128px;
+    letter-spacing: 2.5px;
+    font-family: 'Barlow Semi Condensed', serif;
+
+    @media(max-width: 375px) {
+        margin-bottom: 25px;
+    }
+`;
+
+const RulesModalContainer = styled.div`
+    max-width: 1366px;
+    height: 770px;
+    width: 100%;
+    background-color: rgba(12, 18, 34, 0.85);
+    display: flex;
+    justify-content: center;
+    align-items: center; 
+    position: fixed;
+    top: 0;
+    z-index: 10000000000;
+`;
+
+const RulesContainer = styled.div`
+    background-color: white;
+    border-radius: 10px;
+    padding: 20px 28px;
+    margin-bottom: 2px;
+`;
+
+const RulesTopBanner = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    font-size: 3.0rem;
+    font-family: 'Barlow Semi Condensed', serif;
+    color: hsl(229, 25%, 31%);
+    margin-bottom: 20px;
+    font-weight: 700;
+    letter-spacing: 1.2px;
+    margin-top: 3px;
+    padding: 12px 3px 0 3px;
+`;
+
+const RulesImg = styled.img`
+    padding: 0px 20px;
+    margin-top: 30px;
+    margin-bottom: 10px;
+`;
+
+const CloseIconImg = styled.img`
+    width: 20px;
+    height: 20px;
+`;
+
+export {
+    RulesBottomDiv,
+    SwitchDifficultyButton,
+    ShowRulesButton,
+    RulesModalContainer,
+    RulesContainer,
+    RulesTopBanner,
+    RulesImg,
+    CloseIconImg,
+}
