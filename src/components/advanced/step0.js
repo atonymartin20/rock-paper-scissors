7c297832b330a01aa9763d3c939ec1982aa92a0e
Alex Martin <atonymartin20@yahoo.com>
2020-11-17 20:18:44 -0600

    Advanced Step 0 completed

---
+++ src/components/advanced/step0.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { AdvancedStep0Div, AdvancedRockImg, AdvancedPaperImg, AdvancedScissorsImg, AdvancedSpockImg, AdvancedLizardImg, AdvancedSpockPaperDiv, AdvancedLizardRockDiv, PaperDiv, PaperButton, RockDiv, RockButton, ScissorsDiv, ScissorsButton, SpockDiv, SpockButton, LizardDiv, LizardButton } from '../styledComponents/advanced.js';
+import PaperImg from '../../images/icon-paper.svg';
+import ScissorsImg from '../../images/icon-scissors.svg';
+import RockImg from '../../images/icon-rock.svg';
+import SpockImg from '../../images/icon-spock.svg';
+import LizardImg from '../../images/icon-lizard.svg';
+
+class Step0 extends React.Component {
+    render() {
+        return (
+            <AdvancedStep0Div>
+                <ScissorsDiv onClick={this.props.userChoosesScissors}>
+                    <ScissorsButton>
+                    <AdvancedScissorsImg src={ScissorsImg} alt='Paper Icon' />
+                    </ScissorsButton>
+                </ScissorsDiv>
+                <AdvancedSpockPaperDiv>
+                    <SpockDiv onClick={this.props.userChoosesPaper}>
+                        <SpockButton>
+                            <AdvancedSpockImg src={SpockImg} alt='Spock Icon' />
+                        </SpockButton>
+                    </SpockDiv>
+                    <PaperDiv onClick={this.props.userChoosesPaper}>
+                        <PaperButton>
+                            <AdvancedPaperImg src={PaperImg} alt='Paper Icon' />
+                        </PaperButton>
+                    </PaperDiv>
+                </AdvancedSpockPaperDiv>
+
+                <AdvancedLizardRockDiv>
+                    <LizardDiv onClick={this.props.userChoosesPaper}>
+                        <LizardButton>
+                            <AdvancedLizardImg src={LizardImg} alt='Lizard Icon' />
+                        </LizardButton>
+                    </LizardDiv>
+                    <RockDiv onClick={this.props.userChoosesRock}>
+                    <RockButton>
+                        <AdvancedRockImg src={RockImg} alt='Paper Icon' />
+                    </RockButton>
+                </RockDiv>
+                </AdvancedLizardRockDiv>
+
+            </AdvancedStep0Div>
+        )
+    }
+}
+
+export default Step0;
